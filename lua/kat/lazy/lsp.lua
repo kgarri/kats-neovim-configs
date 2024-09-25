524bec180d6b40ba7e40b735e4844adafb46b387
kgarri <black.hat.kg@gmail.com>
2024-09-24 23:34:49 -0500

    More changes to the configs to get the rust_analyzer to work

--- lua/kat/lazy/lsp.lua
+++ lua/kat/lazy/lsp.lua
@@ -1,6 +1,5 @@
-return{
+return {
     "neovim/nvim-lspconfig",
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'},
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
@@ -13,96 +12,51 @@ return{
         "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
+
     config = function()
-        local lsp_zero = require('lsp-zero')
+    local cmp = require('cmp')
+    local cmp_lsp = require("cmp_nvim_lsp")
+    local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        cmp_lsp.default_capabilities())
 
-        -- lsp_attach is where you enable features that only work
-        -- if there is a language server active in the file
-        local lsp_attach = function(client, bufnr)
-            local opts = {buffer = bufnr}
-
-            vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-            vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-            vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-            vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-            vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-            vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-            vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-            vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-            vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-            vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-        end
-
-        local cmp = require('cmp')
-        local cmp_select = {behavior = cmp.SelectBehavior.Select}
-        cmp.setup({
-            mapping ={
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({select = true}),
-                ['<C-Space>'] = cmp.mapping.complete(),
+    require("fidget").setup({})
+    require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "lua_ls",
+                "rust_analyzer",
+                "gopls",
             },
-            sources = {
-                {name = 'nvim_lsp'},
-                {name = 'buffer'},
-            },
-            formatting = cmp_format,
-
-        })
-
-
-        lsp_zero.extend_lspconfig({
-            sign_text = true,
-            lsp_attach = lsp_attach,
-            capabilities = require('cmp_nvim_lsp').default_capabilities(),
-
-
-        })
-
-        require('mason').setup({})
-        require('mason-lspconfig').setup({
-            enusre_installed = {'lua_ls','rust_analyzer'},
             handlers = {
-                function(server_name)
-                    require('lspconfig')[server_name].setup({})
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        capabilities = capabilities
+                    }
                 end,
 
-                rust_analyzer = function ()
-                    require('lspconfig').rust_analyzer.setup({
-                        server = {
-                            -- on_attach is a callback called when the language server attachs to the buffer
-                            lsp_attach = lsp_attach,
-                            settings = {
-                                ["rust-analyzer"] = {
-                                    imports = {
-                                        granularity ={
-                                            group = "module",
-                                        },
-                                        prefix = "self",
-                                    },
-                                    cargo = {
-                                        buidlScripts = {
-                                            enable = true
-                                        },
-                                    },
-                                    procMacro = {
-                                        enable = true
-                                    },
-                                    chechOnSave = {
-                                        command = "clippy",
-                                    },
-                                },
-
+                zls = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.zls.setup({
+                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+                        settings = {
+                            zls = {
+                                enable_inlay_hints = true,
+                                enable_snippets = true,
+                                warn_style = true,
                             },
                         },
-                        require("rust-tools").setup()
-
                     })
-                end,
+                    vim.g.zig_fmt_parse_errors = 0
+                    vim.g.zig_fmt_autosave = 0
 
+                end,
                 ["lua_ls"] = function()
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
                         settings = {
                             Lua = {
                                 runtime = { version = "Lua 5.1" },
@@ -113,12 +67,65 @@ return{
                         }
                     }
                 end,
+                ["rust_analyzer"] = function ()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.rust_analyzer.setup{
+                        capabilities = capabilities,
+                        settings = {
+                            ["rust-analyzer"] = {
+                                imports = {
+                                    granularity = {
+                                        group = "module",
+                                    },
+                                    prefix = "self",
+                                },
+                                cargo = {
+                                    buildScripts = {
+                                        enable = true,
+                                    },
+                                },
+                                procMacro = {
+                                    enable = true
+                                },
+                            },
+
+                        },
+                    }
+                end
             }
+        })
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' }, -- For luasnip users.
+            }, {
+                    { name = 'buffer' },
+                })
         })
 
-        lsp_zero.setup_servers({'rust_analyzer','lua_ls'})
+        vim.diagnostic.config({
+            -- update_in_insert = true,
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
     end
-}
-
+    }
