f3f9f243bc327b55933107c14059dd983707a940
kgarri <black.hat.kg@gmail.com>
2024-10-18 16:37:02 -0500

    added clangd to the dots

--- lua/kat/lazy/lsp.lua
+++ lua/kat/lazy/lsp.lua
@@ -29,6 +29,7 @@ return {
                 "lua_ls",
                 "rust_analyzer",
                 "pyright",
+                "clangd",
             },
             handlers = {
                 function(server_name) -- default handler (optional)
